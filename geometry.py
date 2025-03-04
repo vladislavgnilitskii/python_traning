d17d8a25b01c9e0d04531284c742017d2077a3f0
vladislav <scwinck@gmail.com>
2025-03-04 12:19:37 +0600

    add folder geom2d, add file __init__.py

--- geometry.py
+++ geometry.py
@@ -1,18 +1,4 @@
-from math import sqrt
-
-class Point:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-    def distance(self, p2):
-        dx = p2.x - self.x
-        dy = p2.y - self.y
-        return sqrt(dx * dx + dy * dy)
-
-    def __eq__(self, other):
-        return self.x == other.x and self.y == other.y
-
+from geom2d.point import Point
 
 a = Point(0,0)
 b = Point(3, 4)
